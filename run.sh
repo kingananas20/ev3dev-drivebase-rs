a4cdea6b6501f363660d6bfb7b8c6901fdbc009c
Jonas Arter <jonasarter@outlook.com>
2025-09-05 15:35:12 +0200

    managed to install rsync on the ev3 it's faster now to transfer

--- run.sh
+++ run.sh
@@ -4,8 +4,8 @@ SERVER="robot"
 REMOTE_DIR="~"
 REMOTE_CMD="cd ~ && ./ev3"
 
-# Copy binary
-scp "$1" "$SERVER:$REMOTE_DIR"
+# Copy binary (in the future install rsync on the ev3 and then copy using rsync)
+rsync -ah --progress -z "$1" "$SERVER:$REMOTE_DIR"
 
 # Execute on EV3
 ssh "$SERVER" "$REMOTE_CMD"
--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ use ev3dev_lang_rust::Ev3Error;
 fn main() -> Result<(), Ev3Error> {
     let left = Motor::new(MotorPort::OutD, Direction::CounterClockwise);
     let right = Motor::new(MotorPort::OutC, Direction::Clockwise);
-    let drivebase = DriveBase::new(left, right, 48.)?;
+    let drivebase = DriveBase::new(left, right, 43.)?;
 
     drivebase.drive(300, 50)?;
     println!("done driving");
